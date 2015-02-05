101a8d4fb516f63e0d672a0ce3913c624b98dd4e
trung nguyen ba <tantrungnb@gmail.com>
2015-02-04 23:58:21 -0600

    .

---
+++ Assets/Scripts/Couple.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+var target : GameObject[];
+var xVelo: float;
+var yVelo: float;
+var index: int;
+var rand : int;
+function Awake() {
+	xVelo = Random.Range(3f,6f);
+	target=new GameObject[3];
+}
+function Start () {
+	this.yVelo=0f;
+	rand = Random.Range(0,3);
+	if (this.index >2 ) {
+		this.xVelo= -this.xVelo;
+	} 
+	var xdistance = target[rand].transform.position.x-this.transform.position.x;
+	var ydistance = target[rand].transform.position.y-this.transform.position.y;
+	this.yVelo=ydistance*this.xVelo/(xdistance);
+}
+
+function FixedUpdate(){
+	this.transform.Translate((Vector3.right*this.xVelo+Vector3.up*this.yVelo)*Time.fixedDeltaTime);
+	
+}
+function setTarget(tar:GameObject[]) {
+		
+		target[0]=tar[0];
+		target[1]=tar[1];
+		target[2]=tar[2];
+	
+}
+function setIndex(ind:int) {
+	this.index =ind;
+}
+function OnTriggerEnter2D(col: Collider2D) {
+	if (col.tag == "Destroys") {
+		
+		GameObject.Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/ReSpawn.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+public var Target : GameObject[];
+public var Couple: GameObject;
+private var delay: float;
+private var rand : int;
+function Awake () {
+	delay = 1.1f;
+	
+	
+}
+function Update () {
+	if (delay <0 ) {
+		rand = Random.Range(0,6);
+		var tars : GameObject[];
+		tars = new GameObject[3];
+		var NewCouple : GameObject;
+		if (rand <3) {
+			tars[0]=Target[3];
+			tars[1]=Target[4];
+			tars[2]=Target[5];
+			
+			
+		}
+		else {
+			tars[0]=Target[0];
+			tars[1]=Target[1];
+			tars[2]=Target[2];
+			
+		
+		}
+		NewCouple = GameObject.Instantiate(Couple,Target[rand].transform.position,Quaternion.identity);
+		NewCouple.SendMessage("setTarget",tars);
+		NewCouple.SendMessage("setIndex",this.rand);
+		delay=1.1f;
+	}
+	else {
+		delay-=Time.deltaTime;
+	}	
+}
